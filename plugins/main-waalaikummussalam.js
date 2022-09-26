0eefe3913bd72e4a63041b2332a9f923cbbe546d
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-09-26 17:37:04 +0700

    Update main-waalaikummussalam.js

--- plugins/main-waalaikummussalam.js
+++ plugins/main-waalaikummussalam.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/a149197765e49d5641f99.jpg', m, { packname: "DITA-BOT\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", author: "Created\nBy Pick Ford\n\n\n\n\n\n\\n\n\n\n\n\n\n\n\n\n\n\n\nn\n\n\n\n\n\n" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/a149197765e49d5641f99.jpg', m, { packname: "DITA-BOT\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", author: "Created\nBy Pick Ford\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" })
 }
 
 handler.customPrefix = /^ass?alam|اَلسَّلاَمُ عَلَيْكُمْ|السلام عليکم$/i // ketik bot (tanpa prefix)
